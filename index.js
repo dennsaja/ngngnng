03ec30fe1d0f325cf3ce103023c7dee5c6ba3b2e
Denis <136689391+dennsaja@users.noreply.github.com>
2024-08-31 22:20:03 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Import packages
 const express = require("express");
 const index = require("./routes/index");
-const home = require("./routes/home");
-const notfound = require("./routes/404");
-const verif = require("./routes/verified");
 
 // Middlewares
 const app = express();
@@ -11,12 +8,9 @@ app.use(express.json());
 
 // Routes
 app.use("/", index);
-app.use("/home", home);
-app.use("/verified", verif);
-app.use("/home/notfound", notfound);
 // 404 handler
 app.use((req, res, next) => {
-    res.redirect("/home/notfound");
+    res.redirect("/error/dennsaja/function/");
   });
 
 // connection
