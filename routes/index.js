a7feaac51b4c757d2d961c22452f6754dc6c15ea
Denis <136689391+dennsaja@users.noreply.github.com>
2024-12-20 21:02:58 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get("/", async (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'home.html'));
-});
-router.get("/ping", async (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'home.html'));
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
